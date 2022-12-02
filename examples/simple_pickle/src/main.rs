c6f1ff15192fb5344d1ac59c0d5db5810359aedc
Ian Clark <idclark13@gmail.com>
2022-12-02 16:40:29 -0500

    basic example module and set_get_dump tests

---
+++ examples/simple_pickle/src/main.rs
@@ -0,0 +1,14 @@
+use rusty_pickle::{DumpPolicy, Pickle, SerializationMethod};
+
+fn main() {
+    let mut db = Pickle::new("test.db", DumpPolicy::Auto, SerializationMethod::Json);
+
+    // set the value 100 to the key 'key1'
+    db.set("key1", &100).unwrap();
+
+    // set the value 1.1 to the key 'key2'
+    db.set("key2", &1.1).unwrap();
+
+    // set the value 'hello world' to the key 'key3'
+    db.set("key3", &String::from("hello world")).unwrap();
+}
--- tests/pickle_key_value_tests.rs
+++ tests/pickle_key_value_tests.rs
@@ -1,4 +1,4 @@
-use rusty_pickle::Pickle;
+pub use rusty_pickle::Pickle;
 
 #[cfg(test)]
 mod tests {
@@ -15,11 +15,29 @@ mod tests {
         );
 
         let num = 100;
+        let string = String::from("hello");
         db.set("num", &num).unwrap();
+        db.set("a string", &string).unwrap();
         // read a num
         assert_eq!(db.get::<i32>("num").unwrap(), num);
+        // read a string
+        assert_eq!(db.get::<String>("a string").unwrap(), "hello");
     }
+
+    #[test]
+    fn test_dump_db() {
+        let mut db = Pickle::new(
+            "test_name",
+            DumpPolicy::UponRequest,
+            rusty_pickle::SerializationMethod::Json,
+        );
+        let num = 100;
+        let string = String::from("hello");
+        db.set("num", &num).unwrap();
+        db.set("a string", &string).unwrap();
+        assert!(db.dump().is_ok())
+    }
+
+    #[test]
+    fn test_load_db() {}
 }
-// #[test]
-//     fn test_get() {}
-// }
