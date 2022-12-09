e5bc232b23f2a244b0fa4631f9e966d94dc13d6a
Ian Clark <idclark13@gmail.com>
2022-12-09 06:52:48 -0700

    quick update to example module

--- examples/simple_pickle/src/main.rs
+++ examples/simple_pickle/src/main.rs
@@ -14,4 +14,20 @@ fn main() {
 
     let num_keys = db.key_count();
     println!("You have inserted {} keys", num_keys);
+
+    let keys = db.list_keys();
+    println!("Now we will print all of the keys in the database");
+    keys.iter().for_each(|k| println!("{}", k));
+
+    println!("Now we will load the json file into a new database and get the values");
+    let db2 = Pickle::load(
+        "test.db",
+        DumpPolicy::UponRequest,
+        SerializationMethod::Json,
+    )
+    .unwrap();
+
+    println!("Getting the value for key: key1");
+    let val1: String = db2.get::<String>("key1").unwrap();
+    println!("The value is {}", val1);
 }
